6946c6262a3d7bd10910d77aa0daa54d72c9fa75
Emanuel <emanuel.sjovall@gmail.com>
2022-05-18 16:22:58 +0200

    Idk man

--- pi/drone.py
+++ pi/drone.py
@@ -3,30 +3,14 @@ from flask_cors import CORS
 import subprocess
 import  requests
 import json
-# from sense_hat import SenseHat
+from time import sleep
+import redis
+from order import Order
 
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
-
-# sense = SenseHat()
-g = (0, 255, 0)
-w = (255, 255, 255)
-
-idle_pixels = [
-    g, g, g, g, g, g, g, w,
-    g, g, g, g, g, g, w, w,
-    g, g, g, g, g, w, w, g,
-    w, g, g, g, w, w, g, g,
-    w, w, g, w, w, g, g, g,
-    g, w, w, w, g, g, g, g,
-    g, g, w, g, g, g, g, g,
-    g, g, g, g, g, g, g, g,
-]
-
-# def display_status(pixels):
-#     sense.set_pixels(pixels)
 
 #Give a unique ID for the drone
 #===================================================================
@@ -55,15 +39,28 @@ with requests.Session() as session:
     resp = session.post(SERVER, json=drone_info)
 #===================================================================
 
-# drone starts set senshat led to be green/idle symbol
-# display_status(idle_pixels)
+def get_coords(self, order):
+    return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
 
-@app.route('/', methods=['POST'])
-def main():
+# maybe here?
+def loopy():
+    while True:
+        sleep(2)
+        nbr = redis_server.llen("OrderQueue")
+        print(f"Orders In Queue: {nbr}")
+        if (nbr > 0):
+            order = redis_server.lpop("OrderQueue")
+            order = json.loads(order, object_hook=Order.from_json)
+            print("sending req to drone cool")
+            coords = get_coords(order)
+            print(coords)
+            smain(coords)
+
+
+def smain(coords):
     with open('data.json', 'r') as f:
         data = json.load(f)
     print("innan coords")
-    coords = request.json
     # Get current longitude and latitude of the drone 
     #===================================================================
     current_longitude = data['long']
@@ -77,7 +74,32 @@ def main():
                                                  '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
                                                  '--id', myID
                     ])
-    return 'New route received'
+
+
+# @app.route('/', methods=['POST'])
+# def main():
+#     with open('data.json', 'r') as f:
+#         data = json.load(f)
+#     print("innan coords")
+#     coords = request.json
+#     # Get current longitude and latitude of the drone 
+#     #===================================================================
+#     current_longitude = data['long']
+#     current_latitude = data['lat']
+#     #===================================================================
+#     from_coord = coords['from']
+#     to_coord = coords['to']
+#     print("innan subprocess")
+#     subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
+#                                                  '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
+#                                                  '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
+#                                                  '--id', myID
+#                     ])
+#     return 'New route received'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
+    loopy()
+

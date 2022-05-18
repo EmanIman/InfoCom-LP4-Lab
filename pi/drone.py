5643d57ebeec666d65f112ef4cac58e3acc0f678
Emanuel <emanuel.sjovall@gmail.com>
2022-05-18 16:42:19 +0200

    test 2

--- pi/drone.py
+++ pi/drone.py
@@ -42,55 +42,53 @@ def get_coords(self, order):
     return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
 
 
-def nmain():
-    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
-    while True:
-        sleep(2)
-        nbr = redis_server.llen("OrderQueue")
-        if (nbr > 0):
-            order = redis_server.lpop("OrderQueue")
-            order = json.loads(order, object_hook=Order.from_json)
-            coords = get_coords(order)
-
-            from_coord = coords['from']
-            to_coord = coords['to']
-
-            subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
-                                                '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
-                                                '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
-                                                '--id', myID
-                            ])
-
-# @app.route('/', methods=['POST'])
-# def main():
+# def nmain():
 #     redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
-#     with open('data.json', 'r') as f:
-#         data = json.load(f)
-#     print("innan coords")
-#     coords = request.json
-#     order = redis_server.lpop("OrderQueue")
-#     order = json.loads(order, object_hook=Order.from_json)
-#     print("sending req to drone cool")
-#     coords = get_coords(order)
-#     print(coords)
+#     while True:
+#         sleep(2)
+#         nbr = redis_server.llen("OrderQueue")
+#         if (nbr > 0):
+#             order = redis_server.lpop("OrderQueue")
+#             order = json.loads(order, object_hook=Order.from_json)
+#             coords = get_coords(order)
 
-#     # Get current longitude and latitude of the drone 
-#     #===================================================================
-#     current_longitude = data['long']
-#     current_latitude = data['lat']
-#     #===================================================================
-#     from_coord = coords['from']
-#     to_coord = coords['to']
-#     print("innan subprocess")
-#     subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
-#                                                  '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
-#                                                  '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
-#                                                  '--id', myID
-#                     ])
-#     return 'New route received'
+#             from_coord = coords['from']
+#             to_coord = coords['to']
+
+#             subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
+#                                                 '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
+#                                                 '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
+#                                                 '--id', myID
+#                             ])
+
+@app.route('/', methods=['POST'])
+def main():
+    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    print("innan coords")
+    order = redis_server.lpop("OrderQueue")
+    order = json.loads(order, object_hook=Order.from_json)
+    print("sending req to drone cool")
+    coords = get_coords(order)
+    print(coords)
+
+    # Get current longitude and latitude of the drone 
+    #===================================================================
+    current_longitude = data['long']
+    current_latitude = data['lat']
+    #===================================================================
+    from_coord = coords['from']
+    to_coord = coords['to']
+    print("innan subprocess")
+    subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
+                                                 '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
+                                                 '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
+                                                 '--id', myID
+                    ])
+    return 'New route received'
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    nmain()
 
--- webserver/DroneCommunicator.py
+++ webserver/DroneCommunicator.py
@@ -1,44 +1,39 @@
-# import redis
-# import json
-# import requests
-# from order import Order
-# from time import sleep
+import redis
+import json
+import requests
+from order import Order
+from time import sleep
 
-# class DroneCommunicator:
-#     @staticmethod
-#     def send_request(drone_url, coords):
-#         with requests.Session() as session:
-#             resp = session.post(drone_url, json=coords)
+class DroneCommunicator:
 
-#     @staticmethod
-#     def get_coords(order):
-#         return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
+    def __init__(self):
+        self.redis_server = redis.Redis("localhost", decode_responses=True, charset="unicode_escape")
+    
+    def send_request(drone_url):
+        with requests.Session() as session:
+            resp = session.post(drone_url)
 
-#     @staticmethod
-#     def queueLoop(redis_server):
-#         nbr = redis_server.llen("OrderQueue")
-#         print(f"Idle loop, Orders In Queue: {nbr}")
-#         if (redis_server.llen("OrderQueue") > 0):
-#             drones = {"Test": '10.11.44.126', "drone124": '10.11.44.124'}
-#             drone = None
-#             for k, v in drones.items():
-#                 print(k)
-#                 info = redis_server.get(k)
-#                 print(info)
-#                 if info != None:
-#                     info = json.loads(info)
-#                     if info['status'] == 'idle':
-#                         drone = v
-#                         break
+    def get_coords(order):
+        return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
+
+    def queueLoop(redis_server):
+        nbr = redis_server.llen("OrderQueue")
+        print(f"Idle loop, Orders In Queue: {nbr}")
+        if (redis_server.llen("OrderQueue") > 0):
+            drones = {"Test": '10.11.44.126', "drone124": '10.11.44.124'}
+            drone = None
+            for k, v in drones.items():
+                print(k)
+                info = redis_server.get(k)
+                print(info)
+                if info != None:
+                    info = json.loads(info)
+                    if info['status'] == 'idle':
+                        drone = v
+                        break
             
-#             if drone != None:
-#                 order = redis_server.lpop("OrderQueue")
-#                 order = json.loads(order, object_hook=Order.from_json)
-#                 print("sending req to drone cool")
-#                 print(order.coordinatesFrom)
-#                 coords = DroneCommunicator.get_coords(order)
-#                 print(coords)
-#                 DroneCommunicator.send_request("http://" + drone + ":5000", coords)
+            if drone != None:
+                DroneCommunicator.send_request("http://" + drone + ":5000")
 
 
 
@@ -46,58 +41,58 @@
 
 # Old stuff
 
-from concurrent.futures import thread
-import redis
-import json
-import requests
-from order import Order
-from time import sleep
-import threading
+# from concurrent.futures import thread
+# import redis
+# import json
+# import requests
+# from order import Order
+# from time import sleep
+# import threading
 
-class DroneCommunicator:
+# class DroneCommunicator:
     
-    def __init__(self):
-        self.redis_server = redis.Redis("localhost", decode_responses=True, charset="unicode_escape")
+#     def __init__(self):
+#         self.redis_server = redis.Redis("localhost", decode_responses=True, charset="unicode_escape")
 
-    def send_request(self, drone_url, coords):
-        with requests.Session() as session:
-            resp = session.post(drone_url, json=coords)
+#     def send_request(self, drone_url, coords):
+#         with requests.Session() as session:
+#             resp = session.post(drone_url, json=coords)
 
-    def get_coords(self, order):
-        return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
+#     def get_coords(self, order):
+#         return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
 
-    def queueLoop(self):
-        while True:
-            sleep(3)
-            nbr = self.redis_server.llen("OrderQueue")
-            print(f"Idle loop, Orders In Queue: {nbr}")
-            if (nbr > 0):
-                drones = {"Test": '10.11.44.126', "drone124": '10.11.44.124'}
-                drone_ip = None
-                for k, v in drones.items():
-                    print(k)
-                    drone_info = self.redis_server.get(k)
-                    print(drone_info)
-                    if drone_info != None:
-                        drone_info = json.loads(drone_info)
-                        if drone_info['status'] == 'idle':
-                            drone_ip = v
-                            break
+#     def queueLoop(self):
+#         while True:
+#             sleep(3)
+#             nbr = self.redis_server.llen("OrderQueue")
+#             print(f"Idle loop, Orders In Queue: {nbr}")
+#             if (nbr > 0):
+#                 drones = {"Test": '10.11.44.126', "drone124": '10.11.44.124'}
+#                 drone_ip = None
+#                 for k, v in drones.items():
+#                     print(k)
+#                     drone_info = self.redis_server.get(k)
+#                     print(drone_info)
+#                     if drone_info != None:
+#                         drone_info = json.loads(drone_info)
+#                         if drone_info['status'] == 'idle':
+#                             drone_ip = v
+#                             break
                 
-                if drone_ip != None:
-                    order = self.redis_server.lpop("OrderQueue")
-                    order = json.loads(order, object_hook=Order.from_json)
-                    print("sending req to drone cool")
-                    print(order.coordinatesFrom)
-                    coords = self.get_coords(order)
-                    print(coords)
-                    # self.send_request("http://" + drone_ip + ":5000", coords)
-                    t = threading.Thread(target=self.send_request, args=["http://" + drone_ip + ":5000", coords])
-                    t.start()
+#                 if drone_ip != None:
+#                     order = self.redis_server.lpop("OrderQueue")
+#                     order = json.loads(order, object_hook=Order.from_json)
+#                     print("sending req to drone cool")
+#                     print(order.coordinatesFrom)
+#                     coords = self.get_coords(order)
+#                     print(coords)
+#                     # self.send_request("http://" + drone_ip + ":5000", coords)
+#                     t = threading.Thread(target=self.send_request, args=["http://" + drone_ip + ":5000", coords])
+#                     t.start()
 
 
 
 
-if __name__ == "__main__":
-    dc = DroneCommunicator()
-    dc.queueLoop()
+# if __name__ == "__main__":
+#     dc = DroneCommunicator()
+#     dc.queueLoop()

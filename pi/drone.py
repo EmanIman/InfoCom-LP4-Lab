5c716301fdf183d4dee789cb801b43d5f4035843
Emanuel <emanuel.sjovall@gmail.com>
2022-05-18 16:26:29 +0200

    new test

--- pi/drone.py
+++ pi/drone.py
@@ -7,7 +7,6 @@ from time import sleep
 import redis
 from order import Order
 
-
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
@@ -42,25 +41,20 @@ with requests.Session() as session:
 def get_coords(self, order):
     return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
 
-# maybe here?
-def loopy():
-    while True:
-        sleep(2)
-        nbr = redis_server.llen("OrderQueue")
-        print(f"Orders In Queue: {nbr}")
-        if (nbr > 0):
-            order = redis_server.lpop("OrderQueue")
-            order = json.loads(order, object_hook=Order.from_json)
-            print("sending req to drone cool")
-            coords = get_coords(order)
-            print(coords)
-            smain(coords)
 
-
-def smain(coords):
+@app.route('/', methods=['POST'])
+def main():
+    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
     with open('data.json', 'r') as f:
         data = json.load(f)
     print("innan coords")
+    # coords = request.json
+    order = redis_server.lpop("OrderQueue")
+    order = json.loads(order, object_hook=Order.from_json)
+    print("sending req to drone cool")
+    coords = get_coords(order)
+    print(coords)
+
     # Get current longitude and latitude of the drone 
     #===================================================================
     current_longitude = data['long']
@@ -74,32 +68,9 @@ def smain(coords):
                                                  '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
                                                  '--id', myID
                     ])
-
-
-# @app.route('/', methods=['POST'])
-# def main():
-#     with open('data.json', 'r') as f:
-#         data = json.load(f)
-#     print("innan coords")
-#     coords = request.json
-#     # Get current longitude and latitude of the drone 
-#     #===================================================================
-#     current_longitude = data['long']
-#     current_latitude = data['lat']
-#     #===================================================================
-#     from_coord = coords['from']
-#     to_coord = coords['to']
-#     print("innan subprocess")
-#     subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
-#                                                  '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
-#                                                  '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
-#                                                  '--id', myID
-#                     ])
-#     return 'New route received'
+    return 'New route received'
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
-    loopy()
 

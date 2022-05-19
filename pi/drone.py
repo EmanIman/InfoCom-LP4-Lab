2d335bb7b8f05b83e18a3a4c881d4d4d91cd572e
Emanuel <emanuel.sjovall@gmail.com>
2022-05-19 14:52:47 +0200

    Drone restored from backup

--- pi/drone.py
+++ pi/drone.py
@@ -3,9 +3,6 @@ from flask_cors import CORS
 import subprocess
 import  requests
 import json
-from time import sleep
-import redis
-from order import Order
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -38,41 +35,14 @@ with requests.Session() as session:
     resp = session.post(SERVER, json=drone_info)
 #===================================================================
 
-def get_coords(self, order):
-    return {'from' : order.coordinatesFrom, 'to' : order.coordinatesTo}
-
-
-# def nmain():
-#     redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
-#     while True:
-#         sleep(2)
-#         nbr = redis_server.llen("OrderQueue")
-#         if (nbr > 0):
-#             order = redis_server.lpop("OrderQueue")
-#             order = json.loads(order, object_hook=Order.from_json)
-#             coords = get_coords(order)
-
-#             from_coord = coords['from']
-#             to_coord = coords['to']
-
-#             subprocess.Popen(["python3", "simulator.py", '--clong', str(current_longitude), '--clat', str(current_latitude),
-#                                                 '--flong', str(from_coord[0]), '--flat', str(from_coord[1]),
-#                                                 '--tlong', str(to_coord[0]), '--tlat', str(to_coord[1]),
-#                                                 '--id', myID
-#                             ])
+# maybe here?
 
 @app.route('/', methods=['POST'])
 def main():
-    redis_server = redis.Redis("10.11.44.125", decode_responses=True, charset="unicode_escape", port="6379")
     with open('data.json', 'r') as f:
         data = json.load(f)
     print("innan coords")
-    order = redis_server.lpop("OrderQueue")
-    order = json.loads(order, object_hook=Order.from_json)
-    print("sending req to drone cool")
-    coords = get_coords(order)
-    print(coords)
-
+    coords = request.json
     # Get current longitude and latitude of the drone 
     #===================================================================
     current_longitude = data['long']
@@ -88,7 +58,5 @@ def main():
                     ])
     return 'New route received'
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
